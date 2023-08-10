3f0f1ddf5062f5b126bb980655c4fdb884a0c71d
lamar <107258472+santospaul1@users.noreply.github.com>
2023-08-10 20:54:16 +0300

    Client.java

--- TCP-Socket/Client.java
+++ TCP-Socket/Client.java
@@ -8,6 +8,7 @@ public class Client {
 
     public static void main(String[] args) {
         try {
+            
             host = InetAddress.getLocalHost();
         } catch (UnknownHostException unEx) {
             System.out.println("Host id not found");
@@ -19,7 +20,9 @@ public class Client {
     private static void accessServer() {
         Socket link = null;
         try {
+            //Step 1: Establish a connection to the server
             link = new Socket(host, PORT);
+            //Step 2: Setup Input and output Strean
             Scanner input = new Scanner(link.getInputStream());
             PrintWriter output = new PrintWriter(link.getOutputStream(), true);
 
@@ -27,18 +30,20 @@ public class Client {
             String message, response;
 
             do {
+                
                 System.out.println("Enter a message");
                 message = userEntry.nextLine();
-
+                //Step 3: Send and receive data
                 output.println(message);
                 response = input.nextLine();
-
+                
                 System.out.println("\nSERVER> " + response);
             } while (!message.equals("***CLOSE***"));
         } catch (IOException ioEx) {
             ioEx.printStackTrace();
         } finally {
             try {
+                //Step 4: Close the connection
                 System.out.println("\nClosing connection");
                 link.close();
             } catch (IOException ioEx) {
