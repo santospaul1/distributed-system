267a673dc02eb72d24a97324dfc432dc31e4eec3
lamar <107258472+santospaul1@users.noreply.github.com>
2023-08-10 21:42:26 +0300

    UDPClient.java

--- UDP Sockets/UDPClient.java
+++ UDP Sockets/UDPClient.java
@@ -11,6 +11,7 @@ public class UDPClient {
 
     public static void main(String[] args) throws IOException {
         try {
+            
             host = InetAddress.getLocalHost();
         } catch (UnknownHostException unEx) {
             System.out.println("Host id not found");
@@ -20,6 +21,7 @@ public class UDPClient {
     }
 
     private static void accessServer() throws IOException {
+        //Step 1: Create a datagram object
         datagramSocket = new DatagramSocket();
 
         Scanner userEntry = new Scanner(System.in);
@@ -29,19 +31,27 @@ public class UDPClient {
             message = userEntry.nextLine();
 
             if (!message.equals("***CLOSE***")) {
+                //Step 2: Create an outgoing datagram
                 outpacket = new DatagramPacket(message.getBytes(), message.length(), host, PORT);
+                //Step 3: Send datagram message
                 datagramSocket.send(outpacket);
 
+                //Step 4: create a buffer for incoming messages
                 buffer = new byte[256];
+                
+                //Step 5: Create a datagram packet for incoming messages
                 inpacket = new DatagramPacket(buffer, buffer.length);
+
+                //Step 6: Accept an incoming datagram
                 datagramSocket.receive(inpacket);
 
+                //Step 7: Retrive the data from the buffer
                 response = new String(inpacket.getData(), 0, inpacket.getLength());
 
                 System.out.println("\n SERVER> " + response);
             }
         } while (!message.equals("***CLOSE***"));
-
+         // Step 8: Close the connection
         datagramSocket.close();
     }
 }
