0d920c1af468c63d20f4f562abdf2f6033955980
lamar <107258472+santospaul1@users.noreply.github.com>
2023-08-10 20:32:51 +0300

    Server.java

--- TCP-Socket/Server.java
+++ TCP-Socket/Server.java
@@ -9,6 +9,7 @@ public class TCP {
     public static void main(String[] args) {
         System.out.println("Opening port... \n");
         try {
+            //Step 1: Create a serverSocket object
             serverSocket = new ServerSocket(PORT);
         } catch (IOException ioEx) {
             System.out.println("Unable to attach to the port");
@@ -23,12 +24,16 @@ public class TCP {
     private static void handleClient() {
         Socket link = null;
         try {
+            //Step 2: Put server into waiting state
             link = serverSocket.accept();
+            //Step 3: Setup input and output stream
             Scanner input = new Scanner(link.getInputStream());
             PrintWriter output = new PrintWriter(link.getOutputStream(), true);
 
             int numMessages = 0;
+            //Step 4: Send and Receive data
             String message = input.nextLine();
+            
             while (!message.equals("***CLOSE***")) {
                 System.out.println("Message received.");
                 numMessages++;
@@ -41,6 +46,7 @@ public class TCP {
             ioEx.printStackTrace();
         } finally {
             try {
+                //Step 5: Close the connection
                 System.out.println("\n* Closing connection");
                 link.close();
             } catch (IOException ioEx) {
